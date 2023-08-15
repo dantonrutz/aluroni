f7dcb0806b2156ec91794d8d1b7ec95f23c77446
dantonrutz <dantonyuri7@gmail.com>
2023-08-15 10:33:19 -0300

    Aplicação finalizada

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,11 +1,57 @@
 import cardapio from "./itens.json";
 import Item from "./Item";
 import styles from "./Itens.module.scss";
+import { useEffect, useState } from "react";
 
-export default function Itens(){
+interface itensProps{
+  busca: string;
+  filtro: number | null;
+  ordenador: string;
+}
+
+export default function Itens(props: itensProps){
+
+  const { busca, filtro, ordenador } = props;
+  const [lista, setLista] = useState(cardapio);
+
+  function testaBusca(title: string){
+    const regex = new RegExp(busca, 'i');
+    return regex.test(title);
+  }
+
+  function testaFiltro(id: number){
+    if (filtro !== null){
+      return filtro === id;
+    }
+    return true;
+  }
+
+  function ordenarPropriedadeCrescente (lista: typeof cardapio, propriedade: "size" | "serving" | "price"){
+    return lista.sort((a, b) => (a[propriedade] > b[propriedade] ? 1 : -1)); 
+    //retorna 1 caso a seja maior e -1 caso b seja maior
+  }
+
+  function ordenar(novaLista : typeof cardapio){
+    switch(ordenador){
+      case 'porcao':
+        return ordenarPropriedadeCrescente(novaLista, 'size');
+      case 'qtd_pessoas':
+        return ordenarPropriedadeCrescente(novaLista, 'serving');
+      case 'preco':
+        return ordenarPropriedadeCrescente(novaLista, 'price');
+      default:
+        return novaLista;
+    }
+  }
+
+  useEffect(() => {
+    const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
+    setLista(ordenar(novaLista));
+  },[busca, filtro, ordenador]) //array de dependencias
+
   return(
     <div className={styles.itens}>
-      {cardapio.map(item => (
+      {lista.map(item => (
           <Item key={item.id} {...item} />
       ))}
     </div>
